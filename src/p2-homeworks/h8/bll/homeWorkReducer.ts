6d430421fcd055af7e1d2cdd725c08f4a8676c97
Red <amekysw@gmail.com>
2022-03-08 22:08:31 +0300

    8 exercise end

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,23 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+
+export const homeWorkReducer = (state: UserType[], action: any): any => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            const newState = [...state].sort((a, b) => {
+                if (a.name > b.name) return 1
+                else if (a.name < b.name) return -1
+                else return 0
+            })
+            return action.payload === "up" ? newState : newState.reverse()
         }
+
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(f => f.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
-}
+};
+
+
